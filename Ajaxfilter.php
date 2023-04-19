82f05943ff1a4610534be3d7c231c836126a5582
sidharthrajwal <131239031+sidharthrajwal@users.noreply.github.com>
2023-04-19 12:32:01 +0530

    Create Ajaxfilter.php

---
+++ Ajaxfilter.php
@@ -0,0 +1,12 @@
+
+<div class="product_tags ">
+		   		   <img  class="list_item" term="newone" value="new" src="--">
+
+		   		   <img class="list_item" term="xnew"src="--">
+
+		   		   <img class="list_item" term="term" src="--">
+
+		   	  </div>
+
+
+<div id="my-custom-filter-results">done</div>
